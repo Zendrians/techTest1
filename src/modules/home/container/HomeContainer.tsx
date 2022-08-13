b9d4464a79eb65895048a56a206401b001054523
Zendrian <zendrian@gmail.com>
2022-08-13 12:43:33 -0500

    fetch data with axios

--- src/modules/home/container/HomeContainer.tsx
+++ src/modules/home/container/HomeContainer.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import HomeLayout from "../components/HomeLayout/HomeLayout";
 
 const HomeContainer: React.FC = () => {
+  useEffect(() => {
+    const fetchPk = async () => {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon/?limit=150"
+      );
+      console.log(response.data);
+    };
+    fetchPk();
+  }, []);
+
   return <HomeLayout />;
 };
 
