415dd04274853f67a510015bb77963c79008b3e9
Zendrian <zendrian@gmail.com>
2022-08-15 18:46:08 -0500

    Add bottom nav

--- src/global/componets/bottomNav/BottomNav.tsx
+++ src/global/componets/bottomNav/BottomNav.tsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import "./BottomNav.scss";
 
 const BottomNav: React.FC = () => {
   return (
     <nav className="bottomNav">
-      <span className="bottomNav--navLink">Home</span>
+      <Link to={"/home"}>
+        <span style={{ cursor: "pointer" }} className="bottomNav--navLink">
+          Home
+        </span>
+      </Link>
       <hr className="bottomNav--divider" />
-      <span className="bottomNav--navLink">Favorites</span>
+      <Link to={"/favorites"}>
+        <span style={{ cursor: "pointer" }} className="bottomNav--navLink">
+          Favorites
+        </span>
+      </Link>
     </nav>
   );
 };
